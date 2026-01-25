c67e4bd2d7eb352fada3777740614e388070a37b
roneosrone-hub <roneosrone@gmail.com>
2026-01-25 18:14:56 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "LFM Agro",
-  description: "Plataforma agrícola inteligente."
+  description: "Plataforma agrícola inteligente.",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
