1c77c9ee33817b48cbdb2177f73c7d5a7bba21fd
roneosrone-hub <roneosrone@gmail.com>
2026-01-26 00:27:50 -0400

    Create page.tsx

---
+++ app/pro/page.tsx
@@ -0,0 +1,10 @@
+import dynamic from "next/dynamic";
+export const dynamic = "force-dynamic";
+
+const ProdutorMapa = dynamic(() => import("../produtor/produtor-mapa"), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <ProdutorMapa />;
+}
