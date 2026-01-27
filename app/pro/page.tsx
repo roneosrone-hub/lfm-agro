d6eb0dcc9f5d72f2ce34f9bdeb9d4487997fa00f
roneosrone-hub <roneosrone@gmail.com>
2026-01-27 12:34:38 -0400

    Update page.tsx

--- app/pro/page.tsx
+++ app/pro/page.tsx
@@ -2,14 +2,8 @@ import dynamic from "next/dynamic";
 
 export const dynamic = "force-dynamic";
 
-const Mapa = dynamic(() => import("../produtor/mapa/page"), {
-  ssr: false,
-});
+const MapaPage = dynamic(() => import("../produtor/mapa/page"), { ssr: false });
 
 export default function Page() {
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Mapa />
-    </div>
-  );
+  return <MapaPage />;
 }
