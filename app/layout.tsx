5dd3949f11a375c8022cfbe3c06b7372f79cdd14
roneosrone-hub <roneosrone@gmail.com>
2026-01-27 12:58:14 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,28 @@
 import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: "LFM Agro",
+  description: "Plataforma de monitoramento agrícola",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-br">
+      <head>
+        {/* Leaflet */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet-draw@1.0.4/dist/leaflet.draw.css"
+        />
+      </head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
